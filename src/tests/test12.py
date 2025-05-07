0bf1c594d1ff89ded40dcd70ca44728473f3cdc9
Nora <gdv.nora@gmail.com>
2025-05-07 16:57:18 -0400

    Deleted test 12

--- src/tests/test12.py
+++ src/tests/test12.py
@@ -1,19 +1,12 @@
-class Point:
-    x: int
-    y: int
+class Number:
+    val: int
 
-    def add1(self: Point) -> int:
-        return self.x + 1
-
-
-class Book:
-    name: bool
-
-    def get_name(self: Book) -> bool:
-        return self.name
+    def set_num(self: Number, new: int) -> Number:
+        self.val = new
+        return self
 
 
-a = Point(5, 6)
-b = Book(True)
-print(a.add1())
-print(b.get_name())
+n = Number(4)
+print(n.val)
+n = n.set_num(5)
+print(n.val)
